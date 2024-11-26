ef852263de885bcfe97f13c0d2085b3415f78f07
araki-shota2351780 <araki.shota0227@gmail.com>
2024-11-26 01:20:02 +0000

    css

--- 5-2/script/script.js
+++ 5-2/script/script.js
@@ -2,7 +2,12 @@ new Vue({
   el: '#app',
   data() {
     return {
-
+      menus: [
+        { label: 'TOP', path: './index.html' },
+        { label: 'ABOUT', path: './about.html' },
+        { label: 'SCHEDULE', path: './schedule.html' },
+        { label: 'CONTACT', path: './contact.html' },
+      ]
     };
   }
 });
